d50e7f42d6ff62f488d9364ad69bb78426b2d0d9
mauramaura <maurizianonnis@gmail.com>
2025-01-12 21:47:42 +0100

    feat/setup historie and basic components

--- pages/Index.vue
+++ pages/Index.vue
@@ -1,10 +1,101 @@
 <template>
-  <div class="flex gap-4 m-4">
-    <Button variant="destructive">Primary Button</Button>
-    <Button>Secondary Button</Button>
+  <div class="m-4">
+    <div class="flex fle gap-4">
+      <Button variant="destructive">Destructive Button</Button>
+      <Button>Primary Button</Button>
+    </div>
+    <div class="w-96 flex flex-col gap-6">
+      <Accordion
+        multiple
+        class="w-full"
+        collapsible
+        :default-value="defaultValue"
+      >
+        <AccordionItem
+          v-for="item in accordionItems"
+          :key="item.value"
+          :value="item.value"
+        >
+          <AccordionTrigger>{{ item.title }}</AccordionTrigger>
+          <AccordionContent>
+            {{ item.content }}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <Pagination
+        v-slot="{ page }"
+        :total="100"
+        :sibling-count="1"
+        show-edges
+        :default-page="2"
+      >
+        <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+          <PaginationFirst />
+          <PaginationPrev />
+
+          <template v-for="(item, index) in items">
+            <PaginationListItem
+              v-if="item.type === 'page'"
+              :key="index"
+              :value="item.value"
+              as-child
+            >
+              <Button
+                class="w-10 h-10 p-0"
+                :variant="item.value === page ? 'default' : 'outline'"
+              >
+                {{ item.value }}
+              </Button>
+            </PaginationListItem>
+            <PaginationEllipsis v-else :key="item.type" :index="index" />
+          </template>
+
+          <PaginationNext />
+          <PaginationLast />
+        </PaginationList>
+      </Pagination>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { Button } from '@/components/ui/button';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+import {
+  Pagination,
+  PaginationEllipsis,
+  PaginationFirst,
+  PaginationLast,
+  PaginationList,
+  PaginationListItem,
+  PaginationNext,
+  PaginationPrev,
+} from '@/components/ui/pagination';
+
+const defaultValue = 'item-1';
+
+const accordionItems = [
+  {
+    value: 'item-1',
+    title: 'Is it accessible?',
+    content: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+  {
+    value: 'item-2',
+    title: 'Is it unstyled?',
+    content:
+      "Yes. It's unstyled by default, giving you freedom over the look and feel.",
+  },
+  {
+    value: 'item-3',
+    title: 'Can it be animated?',
+    content: 'Yes! You can use the transition prop to configure the animation.',
+  },
+];
 </script>
